1a9695d819079f2bfb2f802276dd4d0c79b84590
SebasL07 <sebasgnv0207@gmail.com>
2025-03-23 19:45:24 -0500

    JSL/feat: update user router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,11 +3,12 @@ import { userController } from "../controllers";
 
 export const userRouter = Router();   
 
-userRouter.get('/', userController.getUsers);
+userRouter.get('/users', userController.getUsers);
 userRouter.get('/:id', userController.getUser);
-userRouter.post('/', userController.postUsers);
+userRouter.post('/users', userController.postUsers);
 userRouter.put('/:email', userController.putUserByEmail);
 userRouter.delete('/:email', userController.deleteUsers);
+userRouter.post('/login', userController.logIn);
 
 
 
