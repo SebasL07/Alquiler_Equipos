ab1e53568926e6bd4829fe8935338b2b39991b83
SebasL07 <sebasgnv0207@gmail.com>
2025-03-26 08:20:56 -0500

    update connection to database

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,8 +1,15 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const db = new Sequelize('alquiler', 'root', '', {
+const db = new Sequelize(
+    process.env.DB_NAME || 'alquiler',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || '',
+    {
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: Number(process.env.DB_PORT) || 3306,
 });
 
 export default db;
